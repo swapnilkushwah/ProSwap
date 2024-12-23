6c53b7c1faadb368441d55295d1dce1ac95e8ac1
raghvendrathakur900@gmail.com <raghvendrathakur900@gmail.com>
2024-12-23 11:40:48 +0530

    crate a  date, month,year program

--- SMC/SMC/Source.h
+++ SMC/SMC/Source.h
@@ -2,31 +2,28 @@
 
 class Demo
 {
-	int x;
-	int y;
+   int x;
+   int y;
+
 public:
-	void setData(int x, int y);
-	int getData();
+ 
+ void setData(int x, int y);
+ int getData();
+
 };
 
 
 class Dates{
-
-
-
-
-
+	
 private:  
 
 int day;
 int month;
 int year;
 
-
 public:
 
 void setdate(int day, int month, int year);
 void display();
-
 
 };
